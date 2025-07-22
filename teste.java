0f8dc2a46d959abbe5d988f4cb08131e793498a9
FraNFraN <134552777+BatataDeCodigos@users.noreply.github.com>
2025-07-22 09:45:11 -0300

    Atividade de antes

---
+++ teste.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Calcular{
+  public static void main(String[] args) {
+     //trazendo a classe 
+  Scanner ler = new Scanner(System.in);
+  int x, y, soma;
+    //dados
+  System.out.printf("Informe o primeiro valor:");
+  x = ler.nextInt();
+  System.out.printf("Informe o segundo valor:");
+  y = ler.nextInt();;
+     //processo
+  soma = x + y;
+  //print
+  System.out.print(soma); 
+  ler.close();
+  
+  }
+  }
+
+
+  
